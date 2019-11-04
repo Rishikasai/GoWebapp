6bb71539ca949537d1c55534294892235d66b61c
Rishikasai <48752586+Rishikasai@users.noreply.github.com>
2019-11-04 18:58:52 +0530

    Update main.go

--- Minibank/main.go
+++ Minibank/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
-	"log"
 	"net/http"
+	"os"
 	"strconv"
+
+	"./models"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,10 +22,33 @@ type User struct {
 
 var templates *template.Template
 var store = sessions.NewCookieStore([]byte("rishika"))
-var db *sql.DB
+
+//var db *sql.DB
 var err error
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: true,
+	})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+
+	file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(file)
+	} else {
+		log.Info("Failed to log to file, using default stderr")
+	}
+
+}
+
+var db = models.Init()
+
 func main() {
+
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeGetHandler).Methods("GET")
@@ -40,6 +65,7 @@ func main() {
 	r.HandleFunc("/index", IndexGetHandler).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func homeGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,9 +73,14 @@ func homeGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 func logoutGetHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session")
+	a := session.Values["username"]
 	session.Values["username"] = nil
 	session.Save(r, w)
 	http.Redirect(w, r, "/", 302)
+	log.WithFields(log.Fields{
+		"event": "logout",
+		"user":  a,
+	}).Info("User loggedout")
 }
 func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "login.html", nil)
@@ -57,7 +88,7 @@ func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 
 func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	db, err := sql.Open("mysql", "root:rishika@(127.0.0.1:3306)/dbname")
+
 	username2 := r.PostForm.Get("username")
 	password2 := r.PostForm.Get("password")
 	Result, err := db.Query("SELECT * FROM users WHERE username=?", username2)
@@ -76,20 +107,28 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	}
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password2))
+
 	if err != nil {
 		templates.ExecuteTemplate(w, "login.html", "invalid login")
+		log.WithFields(log.Fields{
+			"event": "logIn",
+			"user":  username2,
+		}).Warn("Login failed. Username and Password doesn't match")
 		return
 	}
 	if err == nil {
 		session, _ := store.Get(r, "session")
 		session.Values["username"] = username2
 		session.Save(r, w)
+		log.WithFields(log.Fields{
+			"event": "logIn",
+			"user":  username2,
+		}).Info("User loggedIn")
+
 		http.Redirect(w, r, "/index", 302)
 
 	}
-
 	templates.ExecuteTemplate(w, "login", nil)
-	defer db.Close()
 }
 
 func registerGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -97,7 +136,7 @@ func registerGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func registerPostHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "root:rishika@(127.0.0.1:3306)/dbname?parseTime=true")
+
 	r.ParseForm()
 	username := r.PostForm.Get("username")
 	password := r.PostForm.Get("password")
@@ -111,8 +150,12 @@ func registerPostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.WithFields(log.Fields{
+		"event": "Register",
+		"user":  username,
+	}).Info("New User got Registered")
+
 	http.Redirect(w, r, "/login", 301)
-	defer db.Close()
 }
 
 func DepositGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -121,7 +164,7 @@ func DepositGetHandler(w http.ResponseWriter, r *http.Request) {
 
 func DepositPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	db, err := sql.Open("mysql", "root:rishika@(127.0.0.1:3306)/dbname")
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -150,6 +193,7 @@ func DepositPostHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		amon := amount2
 		amount2 = amount2 + user.Amount
 		result3, err := db.Prepare("UPDATE users SET amount=? WHERE username=?")
 		if err != nil {
@@ -157,9 +201,14 @@ func DepositPostHandler(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "/index", 302)
 		}
 		result3.Exec(amount2, username2)
+		log.WithFields(log.Fields{
+			"event":  "Deposit",
+			"user":   username2,
+			"amount": amon,
+		}).Info("Amount Deposited")
 	}
 	http.Redirect(w, r, "/index", 302)
-	defer db.Close()
+
 }
 
 func WithdrawGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -167,7 +216,7 @@ func WithdrawGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 func WithdrawPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	db, err := sql.Open("mysql", "root:rishika@(127.0.0.1:3306)/dbname")
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -198,9 +247,15 @@ func WithdrawPostHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		if user.Amount < amount2 {
 			w.Write([]byte("Insufficient amount! Please check balance"))
+			log.WithFields(log.Fields{
+				"event":  "Withdraw",
+				"user":   username2,
+				"amount": amount2,
+			}).Warn("Attempted to withdraw more than the balance")
 			return
 		}
 		if user.Amount >= amount2 {
+			amo := amount2
 			amount2 = user.Amount - amount2
 			result3, err := db.Prepare("UPDATE users SET amount=? WHERE username=?")
 			if err != nil {
@@ -208,13 +263,18 @@ func WithdrawPostHandler(w http.ResponseWriter, r *http.Request) {
 				http.Redirect(w, r, "/index", 302)
 			}
 			result3.Exec(amount2, username2)
+			log.WithFields(log.Fields{
+				"event":  "Withdraw",
+				"user":   username2,
+				"amount": amo,
+			}).Info("Amount Withdrawed")
 		}
 	}
 	http.Redirect(w, r, "/index", 302)
-	defer db.Close()
+
 }
 func BalanceGetHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "root:rishika@(127.0.0.1:3306)/dbname")
+
 	session, _ := store.Get(r, "session")
 	untyped, ok := session.Values["username"]
 	if !ok {
@@ -237,9 +297,13 @@ func BalanceGetHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		user.Amount = amount
 		amount4 := user.Amount
+		log.WithFields(log.Fields{
+			"event": "Balance Check",
+			"user":  username,
+		}).Info("User checked Balance")
 		templates.ExecuteTemplate(w, "balance.html", amount4)
 	}
-	defer db.Close()
+
 }
 func IndexGetHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session")
